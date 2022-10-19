ecae1e17bc831fe23624c11de239e0916ca752fd
Deywerson Pereira <deywerson.pereira@gmail.com>
2022-10-19 19:58:42 -0300

    created redirect after login sucessfully

--- context/AuthContext.tsx
+++ context/AuthContext.tsx
@@ -1,5 +1,12 @@
-import { createContext, ReactNode } from "react";
+import { createContext, ReactNode, useState } from "react";
+import Router from 'next/router';
 import { api } from "../services/api";
+
+type User = {
+  email: string;
+  permissions: string[];
+  roles: string[];
+};
 
 type SignInCredentials = {
   email: string;
@@ -8,6 +15,7 @@ type SignInCredentials = {
 
 type AuthContextData = {
   signIn(credentials: SignInCredentials): Promise<void>;
+  user: User | undefined;
   isAuthenticated: boolean;
 };
 
@@ -18,7 +26,8 @@ type AuthProviderProps = {
 export const AuthContext = createContext({} as AuthContextData);
 
 export const AuthProvider = ({ children }: AuthProviderProps) => {
-  const isAuthenticated = false;
+  const [user, setUser] = useState<User>();
+  const isAuthenticated = !!user;
 
   async function signIn({ email, password }: SignInCredentials) {
     try {
@@ -27,7 +36,19 @@ export const AuthProvider = ({ children }: AuthProviderProps) => {
         password,
       });
 
-      console.log(response.data);
+      const { token, refreshToken, permissions, roles } = response.data;
+
+      //sessionStorage = ao fechar o navegador os dados serão perdidos
+      //localStorage = no next é ruim pois a nossa interface é construída server side e no server side não temos acesso ao objeto window (seria necessário uma adaptação para funcionar)
+      //cookies = é por este motivo que os cookies são mais utilizados no next. Pode ser acessado tanto pelo browser quando pelo servidor 
+
+      setUser({
+        email,
+        permissions,
+        roles,
+      });
+
+      Router.push('/dashboard');
     } catch (err) {
       console.error(err);
     }
@@ -37,7 +58,8 @@ export const AuthProvider = ({ children }: AuthProviderProps) => {
     <AuthContext.Provider
       value={{
         signIn,
-        isAuthenticated
+        isAuthenticated,
+        user,
       }}
     >
       {children}
