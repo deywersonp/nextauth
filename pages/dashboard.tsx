2bc7dacb660513a49cca7f0c9d4e4aef8946809c
Deywerson Pereira <deywerson.pereira@gmail.com>
2022-10-20 21:01:59 -0300

    created apiClient to support both client and ssr cookies

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,9 +1,17 @@
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { AuthContext } from "../context/AuthContext";
+import { setupAPIClient } from "../services/api";
+import { api } from "../services/apiClient";
 import { withSSRAuth } from "../utils/withSSRAuth";
 
 export default function Dashboard() {
   const { user } = useContext(AuthContext);
+
+  useEffect(() => {
+    api.get('/me')
+      .then(response => console.log(response))
+      .catch(err => console.log(err));
+  }, []);
 
   return (
     <h1>Dashboard: {user?.email} </h1>
@@ -11,6 +19,10 @@ export default function Dashboard() {
 };
 
 export const getServerSideProps = withSSRAuth(async (ctx) => {
+  const apiClient = setupAPIClient(ctx);
+  const response = await apiClient.get('/me');
+  console.log(response.data);
+
   return {
     props: {}
   }
